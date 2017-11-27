c432db1c624a12e436bc9d6a77156644f9e3eff0
rPortelas <remy.portelas@gmail.com>
2017-11-27 11:07:36 +0100

    creation of normalized bows

--- RI_2016/src/MainImage.java
+++ RI_2016/src/MainImage.java
@@ -1,8 +1,10 @@
 import java.util.Set;
+import java.util.Iterator;
 import java.util.List;
 import upmc.ri.io.*;
 import upmc.ri.index.*;
 import upmc.ri.struct.*;
+import java.util.ArrayList;
 
 public class MainImage {
 
@@ -10,30 +12,38 @@ public class MainImage {
 		// TODO Auto-generated method stub
 		Set<String> labels = ImageNetParser.classesImageNet();			
 		
-		List <STrainingSample<double[],String>> listTrain ;
-		List <STrainingSample<double[],String>> listTest ;
+		List <STrainingSample<double[],String>> listTrain = new ArrayList<STrainingSample<double[],String>>();
+		List <STrainingSample<double[],String>> listTest = new ArrayList<STrainingSample<double[],String>>();
 		
-		String path_to_bows = "/home/kalifou/Documents/m2/ri/tmes/tme_ri_image/sbow/";
+		//String path_to_bows = "/home/kalifou/Documents/m2/ri/tmes/tme_ri_image/sbow/";
+		String path_to_bows = "/home/rportelas/Documents/RI/tme_ri_image/sbow/";
+		
 		// iterate over set of labels
-		for( String l : labels){
-			List <ImageFeatures> F ;
+		Iterator<String> it = labels.iterator();
+		while (it.hasNext()) {
+			String label = it.next();
+			
+			List <ImageFeatures> F = new ArrayList<ImageFeatures>();
+			System.out.println(label);
 			try {
-				F = ImageNetParser.getFeatures(path_to_bows+ l +".txt");
+				F = ImageNetParser.getFeatures(path_to_bows+ label +".txt");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			int cpt=0;
-			for( ImageFeatures ft : F){
+			Iterator<ImageFeatures> image_it = F.iterator();
+			while (image_it.hasNext()) {
+				ImageFeatures ft = image_it.next();
 				
 				double[] bow = VIndexFactory.computeBow(ft);
-				if(cpt<800){
-					
-					listTrain.add(STrainingSample<bow,ft>);
+				STrainingSample<double[],String> data_sample = new STrainingSample<double[], String>(bow, label);
+				if(cpt<800){	
+					listTrain.add(data_sample);
 				}
 				else{
-					listTest.add(STrainingSample<bow,ft> );
+					listTest.add(data_sample);
 				}
 				cpt+=1;
 				
--- RI_2016/src/upmc/ri/index/VIndexFactory.java
+++ RI_2016/src/upmc/ri/index/VIndexFactory.java
@@ -1,10 +1,26 @@
 package upmc.ri.index;
 import upmc.ri.index.*;
+import java.util.Set;
+import java.util.stream.DoubleStream;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class VIndexFactory {
 	public static double[] computeBow(ImageFeatures ft){
-		double[] L =null;
-		return L;
+		List<Integer> words = ft.getwords();
+		int vocab_size = ImageFeatures.tdico;
+		
+		//create bow (hard assignment / sum pooling)
+		double[] bow = new double[vocab_size];
+		for (int w : words) {
+			bow[w]++;
+		}
+		
+		//l2 norm
+		double sum = DoubleStream.of(bow).sum();
+		bow = Arrays.stream(bow).map(k -> k / sum).toArray();
+		return bow;
 		
 	}
 }
