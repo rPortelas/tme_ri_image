c891694fd93f0a47e0e511c9346d24b0cb459e38
rPortelas <remy.portelas@gmail.com>
2017-12-07 14:19:56 +0100

    Display of conv_matrix and test error comparison done with 0-1 and hierarch. loss

--- RI_2016/src/MulticlassClassif.java
+++ RI_2016/src/MulticlassClassif.java
@@ -18,6 +18,7 @@ public class MulticlassClassif {
 	public static double regul = 10e-6;
 	public static double lr = 10e-2;
 	public static int epochs_nb = 100;
+	public static boolean use_hier_model = false;
 		
 
 	public static void main(String[] args) {
@@ -26,8 +27,13 @@ public class MulticlassClassif {
 		
 		System.out.println("init MultiClass Instantiation");
 		//-----------------------------------------------------------------------------------------
-		//IStructInstantiation<double[],String> Inst = new MultiClass();
-		IStructInstantiation<double[],String> Inst = new MultiClassHier();
+		IStructInstantiation<double[],String> Inst;
+		if(use_hier_model) {
+			Inst = new MultiClassHier();
+		}
+		else {
+			Inst = new MultiClass();
+		}
 		
 		System.out.println("init model");
 		IStructModel<double[],String> model = new LinearStructModel_Ex<double[], String>(input_dim,Inst);
@@ -42,19 +48,39 @@ public class MulticlassClassif {
 		ITrainer<double[],String> Trainer = new SGDTrainer<double[],String>();
 		Trainer.train(dSet.listtrain,model,epochs_nb,lr,regul,Ev);
 		
-		System.out.println("Compute confusion matrix");
+		//System.out.println("Compute confusion matrix");
 		Ev.evaluate();
 		MultiClass MC = (MultiClass) Inst;
+		
+		
 		
 		List<String> test_y = new ArrayList<String>(dSet.listtest.size());
 		for (STrainingSample<double[], String> test_sample : dSet.listtest) {
 			test_y.add(test_sample.output);
 		}
 		
-		//SHOULD RETURN 1
-		//double[][] mat_conf = MC.confusionMatrix(test_y,test_y);
-		
 		double[][] mat_conf = MC.confusionMatrix(Ev.getPred_test(),test_y);
+		
+		//get mean error on test using 0-1 and hierarchical models
+		IStructInstantiation<double[],String> Inst_test_hier = new MultiClassHier();
+		IStructInstantiation<double[],String> Inst_test_01 = new MultiClass();
+		
+		if(use_hier_model) {
+			System.out.println("Using a SVM trained with hierarchical loss: ");
+		}
+		else {
+			System.out.println("Using a SVM trained with 0-1 loss: ");
+		}
+		
+		model.setInst(Inst_test_hier);
+		Ev.setModel(model);
+		Ev.evaluate();
+		System.out.println("Mean error on test set using hierarchical loss = " + Ev.getErr_test());
+		
+		model.setInst(Inst_test_01);
+		Ev.setModel(model);
+		Ev.evaluate();
+		System.out.println("Mean error on test set using 0-1 loss = " + Ev.getErr_test());
 		
 
 	}
